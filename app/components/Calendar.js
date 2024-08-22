6ca6a46053c10c65d2f6d82b1fb530e62942fe76
zan5786 <141464988+zan5786@users.noreply.github.com>
2024-08-22 15:15:55 -0600

    added hours calculation

--- app/components/Calendar.js
+++ app/components/Calendar.js
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import React, { useState, useEffect } from "react";
 import { Calendar, momentLocalizer, Views } from "react-big-calendar";
 import moment from "moment";
@@ -8,126 +8,156 @@ import AddClass from "@/app/components/Addclass";
 import EditClass from "@/app/components/EditClass"; // Import EditClass
 
 export default function CalendarComponent() {
-    const [currentDate, setCurrentDate] = useState(new Date());
-    const [currentView, setCurrentView] = useState(Views.WEEK);
-    const [showAddClassModal, setShowAddClassModal] = useState(false);
-    const [showEditClassModal, setShowEditClassModal] = useState(false); // State for edit class modal
-    const [selectedClassTimes, setSelectedClassTimes] = useState({ startTime: "", endTime: "" });
-    const [events, setEvents] = useState([]);
-    const [selectedEvent, setSelectedEvent] = useState(null); // State for selected event
+  const [currentDate, setCurrentDate] = useState(new Date());
+  const [currentView, setCurrentView] = useState(Views.WEEK);
+  const [showAddClassModal, setShowAddClassModal] = useState(false);
+  const [showEditClassModal, setShowEditClassModal] = useState(false); // State for edit class modal
+  const [selectedClassTimes, setSelectedClassTimes] = useState({
+    startTime: "",
+    endTime: "",
+  });
+  const [events, setEvents] = useState([]);
+  const [selectedEvent, setSelectedEvent] = useState(null); // State for selected event
 
-    moment.updateLocale("en", {
-        week: {
-            dow: 1, // Set week to start on Monday
-        },
-    });
+  moment.updateLocale("en", {
+    week: {
+      dow: 1, // Set week to start on Monday
+    },
+  });
 
-    const localizer = momentLocalizer(moment);
+  const localizer = momentLocalizer(moment);
 
-    useEffect(() => {
-        fetchEvents();
-    }, []);
+  useEffect(() => {
+    fetchEvents();
+  }, []);
 
-    const fetchEvents = async () => {
-        try {
-            const response = await fetch("/api/scheduledclass");
-            if (!response.ok) {
-                throw new Error("Failed to fetch events");
-            }
-            const data = await response.json();
-            setEvents(data.map(event => ({
-                ...event,
-                start: new Date(event.classDatestart),
-                end: new Date(event.classDateend),
-            })));
-        } catch (error) {
-            console.error("Error fetching events:", error);
-        }
-    };
+  const fetchEvents = async () => {
+    try {
+      const response = await fetch("/api/scheduledclass");
+      if (!response.ok) {
+        throw new Error("Failed to fetch events");
+      }
+      const data = await response.json();
+      setEvents(
+        data.map((event) => ({
+          ...event,
+          start: new Date(event.classDatestart),
+          end: new Date(event.classDateend),
+        })),
+      );
+    } catch (error) {
+      console.error("Error fetching events:", error);
+    }
+  };
 
-    const handleNavigate = (date) => {
-        setCurrentDate(date);
-    };
+  const handleNavigate = (date) => {
+    setCurrentDate(date);
+  };
 
-    const handleSelectSlot = (slotInfo) => {
-        if (currentView === Views.WEEK) {
-            setCurrentDate(slotInfo.start);
-            setCurrentView(Views.DAY);
-        } else if (currentView === Views.DAY) {
-            const startTime = moment(slotInfo.start).format('YYYY-MM-DDTHH:mm');
-            setSelectedClassTimes({ startTime });
-            setShowAddClassModal(true);
-        }
-    };
+  const handleSelectSlot = (slotInfo) => {
+    if (currentView === Views.WEEK) {
+      setCurrentDate(slotInfo.start);
+      setCurrentView(Views.DAY);
+    } else if (currentView === Views.DAY) {
+      const startTime = moment(slotInfo.start).format("YYYY-MM-DDTHH:mm");
+      setSelectedClassTimes({ startTime });
+      setShowAddClassModal(true);
+    }
+  };
 
-    const handleSelectEvent = (event) => {
-        setSelectedEvent(event);
-        setShowEditClassModal(true); // Show edit class modal when an event is clicked
-    };
+  const handleSelectEvent = (event) => {
+    setSelectedEvent(event);
+    setShowEditClassModal(true); // Show edit class modal when an event is clicked
+  };
 
-    const handleUpdateEvent = (updatedEvent) => {
-        setEvents((prevEvents) => prevEvents.map(event =>
-            event.id === updatedEvent.id ? updatedEvent : event
-        ));
-    };
-
-    const refreshClasses = () => {
-        fetchEvents();
-    };
-
-    // Function to apply conditional styling
-    const eventPropGetter = (event) => {
-        const backgroundColor = event.status === "BOOKED_OFF" ? '#6b7280' : '#3b82f6'; // Grey for booked off, blue for others
-        return {
-            style: {
-                backgroundColor: backgroundColor,
-                color: 'white',
-                padding: '5px',
-                borderRadius: '4px',
-            },
-        };
-    };
-
-    return (
-        <div>
-            <div className="flex flex-col items-center justify-center h-screen">
-                <Toolbar
-                    date={currentDate}
-                    view={currentView}
-                    onNavigate={handleNavigate}
-                    onViewChange={setCurrentView}
-                />
-                <div className="w-full p-4">
-                    <Calendar
-                        localizer={localizer}
-                        view={currentView}
-                        views={['week', 'day']}
-                        date={currentDate}
-                        events={events}
-                        style={{ height: "75vh", width: "100%" }}
-                        toolbar={false}
-                        onNavigate={handleNavigate}
-                        onSelectSlot={handleSelectSlot}
-                        onSelectEvent={handleSelectEvent} // Add event handler
-                        selectable
-                        eventPropGetter={eventPropGetter} // Apply the conditional styling
-                    />
-                </div>
-                <AddClass
-                    showModal={showAddClassModal}
-                    setShowModal={setShowAddClassModal}
-                    refreshClasses={refreshClasses}
-                    startTime={selectedClassTimes.startTime}
-                />
-                {selectedEvent && (
-                    <EditClass
-                        showModal={showEditClassModal}
-                        setShowModal={setShowEditClassModal}
-                        refreshClasses={refreshClasses}
-                        classDetails={selectedEvent}
-                    />
-                )}
-            </div>
-        </div>
+  const handleUpdateEvent = (updatedEvent) => {
+    setEvents((prevEvents) =>
+      prevEvents.map((event) =>
+        event.id === updatedEvent.id ? updatedEvent : event,
+      ),
     );
+  };
+
+  const refreshClasses = () => {
+    fetchEvents();
+  };
+
+  // Function to apply conditional styling
+  const eventPropGetter = (event) => {
+    const backgroundColor =
+      event.status === "BOOKED_OFF" ? "#6b7280" : "#3b82f6"; // Grey for booked off, blue for others
+    return {
+      style: {
+        backgroundColor: backgroundColor,
+        color: "white",
+        padding: "5px",
+        borderRadius: "4px",
+      },
+    };
+  };
+
+  const openHours = {
+    Monday: { start: "16:30", end: "20:30" },
+    Tuesday: null,
+    Wednesday: { start: "16:30", end: "20:30" },
+    Thursday: null,
+    Friday: { start: "16:30", end: "20:30" },
+    Saturday: { start: "12:30", end: "16:30" },
+    Sunday: null,
+  };
+
+  const availableHours = (date) => {
+    return {
+      min: new Date(date.setHours(16, 0)), // Midnight
+      max: new Date(date.setHours(23, 59)), // End of the day
+    };
+  };
+
+  const { min, max } = availableHours(currentDate);
+
+  return (
+    <div>
+      <div className="flex flex-col items-center justify-center h-screen">
+        <Toolbar
+          date={currentDate}
+          view={currentView}
+          onNavigate={handleNavigate}
+          onViewChange={setCurrentView}
+          events={events}
+        />
+        <div className="w-full p-4">
+          <Calendar
+            localizer={localizer}
+            view={currentView}
+            views={["week", "day"]}
+            date={currentDate}
+            events={events}
+            style={{ height: "75vh", width: "100%" }}
+            toolbar={false}
+            onNavigate={handleNavigate}
+            onSelectSlot={handleSelectSlot}
+            onSelectEvent={handleSelectEvent} // Add event handler
+            selectable
+            eventPropGetter={eventPropGetter} // Apply the conditional styling
+            min={min}
+            max={max}
+          />
+        </div>
+        <AddClass
+          showModal={showAddClassModal}
+          setShowModal={setShowAddClassModal}
+          refreshClasses={refreshClasses}
+          startTime={selectedClassTimes.startTime}
+        />
+        {selectedEvent && (
+          <EditClass
+            showModal={showEditClassModal}
+            setShowModal={setShowEditClassModal}
+            refreshClasses={refreshClasses}
+            classDetails={selectedEvent}
+          />
+        )}
+      </div>
+    </div>
+  );
 }
